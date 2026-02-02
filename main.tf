af46f265df6cdbd6c92eff3430fb6e692f956e5d
zimisele <zimisele9@gmail.com>
2026-02-02 09:53:25 +0200

    added my first terraform pipeline

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "s3" {
+    bucket = "mys3-prac-demos"
+    key    = "demo/terraform-state-file"
+    region = "eu-west-1"
+    use_lockfile = true
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+    source = "hashicorp/aws"
+    version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = eu-west-1
+}
+
+resource "aws_instance" "github" {
+  ami = "ami-03446a3af42c5e74e"
+  instance_type = "m5.large"
+  subnet_id = "subnet-0c061f458755eddff"
+  vpc_security_group_ids = [ "sg-034e2601c3c1c1b23" ]
+  tags = {
+    Name = "github-test"
+    Env = "Dev"
+  }
+}
