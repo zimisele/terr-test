88a675ad829034dc5d0b248202762de8a16277dc
zimisele <zimisele9@gmail.com>
2026-02-02 10:26:16 +0200

    added the tls version

--- main.tf
+++ main.tf
@@ -29,3 +29,13 @@ resource "aws_instance" "github" {
     Env = "Dev"
   }
 }
+resource "aws_instance" "demos" {
+  ami = "ami-03446a3af42c5e74e"
+  instance_type = "m5.xlarge"
+  subnet_id = "subnet-0c061f458755eddff"
+  vpc_security_group_ids = [ "sg-034e2601c3c1c1b23" ]
+  tags = {
+    Name = "Deploy"
+    Env = "Prod"
+  }
+}
